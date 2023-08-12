709e12b8fae6176dd5d9b16c59a63d1bfd39e4df
PGT3RS <liezhaors6@163.com>
2023-08-12 20:28:47 +0200

    reduce the order of magnitude and final_version_1 works use TriangleMeshShapeSphere to generate sphere

--- tests/user_examples/wlz_3d_viscoplastic_drop_gravity_2Ball/wlz_3d_viscoplastic_drop_gravity_2Ball.cpp
+++ tests/user_examples/wlz_3d_viscoplastic_drop_gravity_2Ball/wlz_3d_viscoplastic_drop_gravity_2Ball.cpp
@@ -9,9 +9,9 @@ using namespace SPH;   // Namespace cite here.
 //----------------------------------------------------------------------
 //	Basic geometry parameters and numerical setup.
 //----------------------------------------------------------------------
-Real DL = 0.2; 
-Real DH = 0.6; 
-Real resolution_ref = 0.005;   
+Real DL = 0.3;
+Real DH = 0.6;
+Real resolution_ref = 0.005; 
 Real BW = resolution_ref * 4;
 BoundingBox system_domain_bounds(Vec3d(-DL, -DL, -BW), Vec3d(DL, DL, DH));
 Real ball_radius = resolution_ref * 10;
@@ -21,12 +21,13 @@ Real ball_radius = resolution_ref * 10;
 Real gravity_g = 1.0;
 Real rho0_s = 1.0e3;                                                                                    /* ρ density. kg/m^3 */
 Real Bulk_modulus = 1.09e5;                                                                             /* κ bulk modulus. Pa */
-Real Shear_modulus = 1.12e4;                                                                            /* μ/G shear modulus. Pa */
-Real yield_stress = 0.1;                                                                                /* σ_Y yield stress. Pa */
+Real Shear_modulus = 1.12e4;                                                                            /* μ/G shear modulus. Pa !!!adjust */
+Real yield_stress = 0.1;                                                                                /* σ_Y yield stress. Pa !!!adjust */
 Real viscous_modulus = 10.0;                                                                            /* η viscosity.  */
 Real Youngs_modulus = (9.0 * Shear_modulus * Bulk_modulus) / (3.0 * Bulk_modulus + Shear_modulus);      /* E Young's modulus. Pa. 此处3.25e4 */
 Real poisson = (3.0 * Bulk_modulus - 2.0 * Shear_modulus) / (6.0 * Bulk_modulus + 2.0 * Shear_modulus); /* ν Poisson's ratio. 取值(-1,0.5). 此处0.45 */
-Real Herschel_Bulkley_power = 1.0; // oobleck with 2.8 or viscoplastic with 1.0                         /* h Herschel_Bulkley_power. */
+Real Herschel_Bulkley_power_1 = 1.0;                                                                    /* h Herschel_Bulkley_power. */
+Real Herschel_Bulkley_power_2 = 2.8;                                                                    /* h Herschel_Bulkley_power. */
 //----------------------------------------------------------------------
 //	Geometric shapes
 //----------------------------------------------------------------------
@@ -35,17 +36,26 @@ class WallBoundary : public ComplexShape
   public:
     explicit WallBoundary(const std::string &shape_name) : ComplexShape(shape_name)
     {
-        Vec3d halfsize_wall(DL, DL, BW / 2.0);     
+        Vec3d halfsize_wall(0.5 * DL, DL, BW / 2.0);     
         Vec3d translation_wall(0.0, 0.0, 0.0); 
         add<TriangleMeshShapeBrick>(halfsize_wall, 10, translation_wall);
     }
 };
-class BallBody : public ComplexShape
+class BallOne : public ComplexShape
 {
   public:
-    explicit BallBody(const std::string &shape_name) : ComplexShape(shape_name)
+    explicit BallOne(const std::string &shape_name) : ComplexShape(shape_name)
     {
-        Vec3d translation_ball(0.0, 0.0, 0.5 * DH);
+        Vec3d translation_ball(0.0, - 0.5 * DL, 0.5 * DH);
+        add<TriangleMeshShapeSphere>(ball_radius, 5, translation_ball);
+    }
+};
+class BallTwo : public ComplexShape
+{
+  public:
+    explicit BallTwo(const std::string &shape_name) : ComplexShape(shape_name)
+    {
+        Vec3d translation_ball(0.0, 0.5 * DL, 0.5 * DH);
         add<TriangleMeshShapeSphere>(ball_radius, 5, translation_ball);
     }
 };
@@ -67,14 +77,21 @@ int main(int ac, char *av[])
     //----------------------------------------------------------------------
     //	Creating body, materials and particles.
     //----------------------------------------------------------------------
-    // SolidBody ball(sph_system, makeShared<GeometricShapeBall>(ball_center, ball_radius, "BallBody"));
-    SolidBody ball(sph_system, makeShared<BallBody>("BallBody"));
-    ball.defineBodyLevelSetShape();
-    ball.defineParticlesAndMaterial<ElasticSolidParticles, ViscousPlasticSolid>(rho0_s, Youngs_modulus, poisson,
-    yield_stress, viscous_modulus, Herschel_Bulkley_power);
+    SolidBody ball_1(sph_system, makeShared<BallOne>("BallOne"));
+    ball_1.defineBodyLevelSetShape();
+    ball_1.defineParticlesAndMaterial<ElasticSolidParticles, ViscousPlasticSolid>(rho0_s, Youngs_modulus, poisson,
+    yield_stress, viscous_modulus, Herschel_Bulkley_power_1);
     (!sph_system.RunParticleRelaxation() && sph_system.ReloadParticles())
-        ? ball.generateParticles<ParticleGeneratorReload>(io_environment, ball.getName())
-        : ball.generateParticles<ParticleGeneratorLattice>();
+        ? ball_1.generateParticles<ParticleGeneratorReload>(io_environment, ball_1.getName())
+        : ball_1.generateParticles<ParticleGeneratorLattice>();
+
+    SolidBody ball_2(sph_system, makeShared<BallTwo>("BallTwo"));
+    ball_2.defineBodyLevelSetShape();
+    ball_2.defineParticlesAndMaterial<ElasticSolidParticles, ViscousPlasticSolid>(rho0_s, Youngs_modulus, poisson,
+    yield_stress, viscous_modulus, Herschel_Bulkley_power_2);
+    (!sph_system.RunParticleRelaxation() && sph_system.ReloadParticles())
+        ? ball_2.generateParticles<ParticleGeneratorReload>(io_environment, ball_2.getName())
+        : ball_2.generateParticles<ParticleGeneratorLattice>();
 
     SolidBody wall_boundary(sph_system, makeShared<WallBoundary>("WallBoundary"));
     wall_boundary.defineParticlesAndMaterial<SolidParticles, Solid>(rho0_s, Youngs_modulus, poisson);
@@ -87,21 +104,25 @@ int main(int ac, char *av[])
         //----------------------------------------------------------------------
         //	Define body relation map used for particle relaxation.
         //----------------------------------------------------------------------
-        InnerRelation ball_inner(ball);
+        InnerRelation ball_1_inner(ball_1);
+        InnerRelation ball_2_inner(ball_2);
         //----------------------------------------------------------------------
         //	Define the methods for particle relaxation for ball.
         //----------------------------------------------------------------------
-        SimpleDynamics<RandomizeParticlePosition> ball_random_particles(ball);
-        relax_dynamics::RelaxationStepInner ball_relaxation_step_inner(ball_inner);
+        SimpleDynamics<RandomizeParticlePosition> ball_1_random_particles(ball_1);
+        relax_dynamics::RelaxationStepInner ball_1_relaxation_step_inner(ball_1_inner);
+        SimpleDynamics<RandomizeParticlePosition> ball_2_random_particles(ball_2);
+        relax_dynamics::RelaxationStepInner ball_2_relaxation_step_inner(ball_2_inner);
         //----------------------------------------------------------------------
         //	Output for particle relaxation.
         //----------------------------------------------------------------------
         BodyStatesRecordingToVtp write_ball_state(io_environment, sph_system.real_bodies_);
-        ReloadParticleIO write_particle_reload_files(io_environment, ball);
+        ReloadParticleIO write_particle_reload_files(io_environment, {&ball_1, &ball_2});
         //----------------------------------------------------------------------
         //	Particle relaxation starts here.
         //----------------------------------------------------------------------
-        ball_random_particles.exec(0.25);
+        ball_1_random_particles.exec(0.25);
+        ball_2_random_particles.exec(0.25);
         write_ball_state.writeToFile(0);
         //----------------------------------------------------------------------
         //	From here iteration for particle relaxation begins.
@@ -110,7 +131,8 @@ int main(int ac, char *av[])
         int relax_step = 1000;
         while (ite < relax_step)
         {
-            ball_relaxation_step_inner.exec();
+            ball_1_relaxation_step_inner.exec();
+            ball_2_relaxation_step_inner.exec();
             ite += 1;
             if (ite % 100 == 0)
             {
@@ -127,22 +149,31 @@ int main(int ac, char *av[])
     //	The contact map gives the topological connections between the bodies.
     //	Basically the range of bodies to build neighbor particle lists.
     //----------------------------------------------------------------------
-    InnerRelation ball_inner(ball);
-    SurfaceContactRelation ball_contact(ball, {&wall_boundary});
+    InnerRelation ball_1_inner(ball_1);
+    SurfaceContactRelation ball_1_contact(ball_1, {&wall_boundary});
+    InnerRelation ball_2_inner(ball_2);
+    SurfaceContactRelation ball_2_contact(ball_2, {&wall_boundary});
     //----------------------------------------------------------------------
     //	Define the main numerical methods used in the simulation.
     //	Note that there may be data dependence on the constructors of these methods.
     //----------------------------------------------------------------------
     SharedPtr<Gravity> gravity_ptr = makeShared<Gravity>(Vec3d(0.0, 0.0, -gravity_g));
-    SimpleDynamics<TimeStepInitialization> ball_initialize_timestep(ball, gravity_ptr);
-    InteractionWithUpdate<CorrectedConfigurationInner> ball_corrected_configuration(ball_inner);
-    ReduceDynamics<solid_dynamics::AcousticTimeStepSize> ball_get_time_step_size(ball, 0.1);
+    SimpleDynamics<TimeStepInitialization> ball_1_initialize_timestep(ball_1, gravity_ptr);
+    SimpleDynamics<TimeStepInitialization> ball_2_initialize_timestep(ball_2, gravity_ptr);
+    InteractionWithUpdate<CorrectedConfigurationInner> ball_1_corrected_configuration(ball_1_inner);
+    InteractionWithUpdate<CorrectedConfigurationInner> ball_2_corrected_configuration(ball_2_inner);
+    ReduceDynamics<solid_dynamics::AcousticTimeStepSize> ball_1_get_time_step_size(ball_1, 0.1);
+    ReduceDynamics<solid_dynamics::AcousticTimeStepSize> ball_2_get_time_step_size(ball_2, 0.1);
     /** stress relaxation for the ball. */
-    Dynamics1Level<solid_dynamics::PlasticIntegration1stHalf> ball_stress_relaxation_first_half(ball_inner);
-    Dynamics1Level<solid_dynamics::Integration2ndHalf> ball_stress_relaxation_second_half(ball_inner);
+    Dynamics1Level<solid_dynamics::PlasticIntegration1stHalf> ball_1_stress_relaxation_first_half(ball_1_inner);
+    Dynamics1Level<solid_dynamics::Integration2ndHalf> ball_1_stress_relaxation_second_half(ball_1_inner);
+    Dynamics1Level<solid_dynamics::PlasticIntegration1stHalf> ball_2_stress_relaxation_first_half(ball_2_inner);
+    Dynamics1Level<solid_dynamics::Integration2ndHalf> ball_2_stress_relaxation_second_half(ball_2_inner);
     /** Algorithms for solid-solid contact. */
-    InteractionDynamics<solid_dynamics::ContactDensitySummation> ball_update_contact_density(ball_contact);
-    InteractionDynamics<solid_dynamics::ContactForceFromWall> ball_compute_solid_contact_forces(ball_contact);
+    InteractionDynamics<solid_dynamics::ContactDensitySummation> ball_1_update_contact_density(ball_1_contact);
+    InteractionDynamics<solid_dynamics::ContactForceFromWall> ball_1_compute_solid_contact_forces(ball_1_contact);
+    InteractionDynamics<solid_dynamics::ContactDensitySummation> ball_2_update_contact_density(ball_2_contact);
+    InteractionDynamics<solid_dynamics::ContactForceFromWall> ball_2_compute_solid_contact_forces(ball_2_contact);
     //----------------------------------------------------------------------
     //	Define the methods for I/O operations and observations of the simulation.
     //----------------------------------------------------------------------
@@ -153,7 +184,8 @@ int main(int ac, char *av[])
     //----------------------------------------------------------------------
     sph_system.initializeSystemCellLinkedLists();
     sph_system.initializeSystemConfigurations();
-    ball_corrected_configuration.exec();
+    ball_1_corrected_configuration.exec();
+    ball_2_corrected_configuration.exec();
     //----------------------------------------------------------------------
     //	Initial states output.
     //----------------------------------------------------------------------
@@ -183,22 +215,33 @@ int main(int ac, char *av[])
             Real relaxation_time = 0.0;
             while (relaxation_time < Dt)
             {
-                ball_initialize_timestep.exec();
+                ball_1_initialize_timestep.exec();
+                ball_2_initialize_timestep.exec();
                 if (ite % 100 == 0)
                 {
                     std::cout << "N=" << ite << " Time: "
                               << GlobalStaticVariables::physical_time_ << " dt: " << dt << "\n";
                 }
-                ball_update_contact_density.exec();
-                ball_compute_solid_contact_forces.exec();
-                ball_stress_relaxation_first_half.exec(dt);
-                ball_stress_relaxation_second_half.exec(dt);
+                ball_1_update_contact_density.exec();
+                ball_1_compute_solid_contact_forces.exec();
+                ball_1_stress_relaxation_first_half.exec(dt);
+                ball_1_stress_relaxation_second_half.exec(dt);
 
-                ball.updateCellLinkedList();
-                ball_contact.updateConfiguration();
+                ball_1.updateCellLinkedList();
+                ball_1_contact.updateConfiguration();
+
+                ball_2_update_contact_density.exec();
+                ball_2_compute_solid_contact_forces.exec();
+                ball_2_stress_relaxation_first_half.exec(dt);
+                ball_2_stress_relaxation_second_half.exec(dt);
+
+                ball_2.updateCellLinkedList();
+                ball_2_contact.updateConfiguration();
 
                 ite++;
-                dt = ball_get_time_step_size.exec();
+                Real dt_1 = ball_1_get_time_step_size.exec();
+                Real dt_2 = ball_2_get_time_step_size.exec();
+                dt = SMIN(dt_1, dt_2);
                 relaxation_time += dt;
                 integration_time += dt;
                 GlobalStaticVariables::physical_time_ += dt;
