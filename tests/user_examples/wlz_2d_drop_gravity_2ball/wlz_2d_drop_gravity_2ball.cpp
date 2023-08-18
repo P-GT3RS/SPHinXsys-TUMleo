c54367b1814e684b9da9c0781cbd31acebd10c91
PGT3RS <liezhaors6@163.com>
2023-08-18 10:12:20 +0200

    final_version

--- tests/user_examples/wlz_2d_drop_gravity_2ball/wlz_2d_drop_gravity_2ball.cpp
+++ tests/user_examples/wlz_2d_drop_gravity_2ball/wlz_2d_drop_gravity_2ball.cpp
@@ -1,36 +1,34 @@
 /**
- * @file 	collision_viscoplastic.cpp
- * @brief 	two balls with viscoplastic and oobleck material bouncing with a boundary
- * @details 
+ * @file 	drop_gravity_2D.cpp
+ * @details two balls with viscoplastic and oobleck(shear thickening) material bouncing with a boundary wall
  * @author 	Liezhao Wu, Chi Zhang and Xiangyu Hu
  */
-#include "sphinxsys.h" // SPHinXsys Library.
-using namespace SPH;   // Namespace cite here.
+#include "sphinxsys.h"                   /* SPHinXsys Library. */
+using namespace SPH;                     /* Namespace cite here. */
 //----------------------------------------------------------------------
 //	Basic geometry parameters and numerical setup.
 //----------------------------------------------------------------------
-Real DL = 0.8;                /**< box length. */
-Real DH = 0.6;                /**< box height. */
-Real resolution_ref = 0.002;  /**< reference resolution. */
-Real ball_radius = resolution_ref * 25;
-Vec2d ball_1_center(0.3 * DL, 0.5 * DH);
-Vec2d ball_2_center(0.7 * DL, 0.5 * DH);
-Real BW = resolution_ref * 4; /**< wall width for BCs. */
-/** Domain bounds of the system. */
-BoundingBox system_domain_bounds(Vec2d(-BW, -BW), Vec2d(DL + BW, DH + BW));
+Real resolution_ref = 0.002;             /* reference resolution. */
+Real DL = 0.8;                           /* boundary wall length. */
+Real DH = 0.4;                           /* height. */
+Real BW = resolution_ref * 4.0;          /* boundary wall width. */
+Real ball_radius = resolution_ref * 25.0;/* ball radius. */
+Vec2d ball_1_center(0.3 * DL, 0.75 * DH);/* ball 1 center. */
+Vec2d ball_2_center(0.7 * DL, 0.75 * DH);/* ball 2 center. */
+BoundingBox system_domain_bounds(Vec2d(0.0, -BW), Vec2d(DL, DH));
 //----------------------------------------------------------------------
-//	Global parameters on material properties
+//	Global parameters on material properties.
 //----------------------------------------------------------------------
-Real gravity_g = 1.5;
-Real rho0_s = 1.0e3;
-Real Bulk_modulus = 1.09e5;
-Real Shear_modulus = 1.12e4;
-Real poisson = (3.0 * Bulk_modulus - 2.0 * Shear_modulus) / (6.0 * Bulk_modulus + 2.0 * Shear_modulus);
-Real Youngs_modulus = (9.0 * Shear_modulus * Bulk_modulus) / (3.0 * Bulk_modulus + Shear_modulus);
-Real yield_stress = 0.1;
-Real viscous_modulus = 10.0;  
-Real Herschel_Bulkley_power_1 = 1.0; /** viscoplastic material. */
-Real Herschel_Bulkley_power_2 = 2.8; /** shear thickening material. */
+Real gravity_g = 2.0;
+Real rho0_s = 1.0e3;                     /* ¦Ñ density. kg/m^3 */
+Real Bulk_modulus = 1.09e5;              /* ¦Ê bulk modulus. Pa */
+Real Shear_modulus = 1.12e4;             /* ¦Ì/G shear modulus. Pa */
+Real Youngs_modulus = (9.0 * Shear_modulus * Bulk_modulus) / (3.0 * Bulk_modulus + Shear_modulus);     /* E Young's modulus. Pa */
+Real poisson = (3.0 * Bulk_modulus - 2.0 * Shear_modulus) / (6.0 * Bulk_modulus + 2.0 * Shear_modulus);/* ¦Í Poisson's ratio. */
+Real yield_stress = 0.1;                 /* ¦Ò_Y yield stress. Pa */
+Real viscosity = 10.0;                   /* ¦Ç viscosity. */
+Real Herschel_Bulkley_power_1 = 1.0;     /* viscoplastic material. */
+Real Herschel_Bulkley_power_2 = 2.8;     /* oobleck(shear thickening) material. */
 //----------------------------------------------------------------------
 //	Geometric shapes
 //----------------------------------------------------------------------
@@ -45,6 +43,7 @@ class WallBoundary : public MultiPolygonShape
         wall_boundary_shape.push_back(Vecd(DL, 0.0));
         wall_boundary_shape.push_back(Vecd(DL, -BW));
         wall_boundary_shape.push_back(Vecd(0.0, -BW));
+
         multi_polygon_.addAPolygon(wall_boundary_shape, ShapeBooleanOps::add);
     }
 };
@@ -74,7 +73,7 @@ int main(int ac, char *av[])
     //----------------------------------------------------------------------
     SPHSystem sph_system(system_domain_bounds, resolution_ref);
     /** Tag for running particle relaxation for the initially body-fitted distribution */
-    sph_system.setRunParticleRelaxation(false); // false
+    sph_system.setRunParticleRelaxation(false); /* true/false */
     /** Tag for starting with relaxed body-fitted particles distribution */
     sph_system.setReloadParticles(true);
     sph_system.handleCommandlineOptions(ac, av);
@@ -85,7 +84,7 @@ int main(int ac, char *av[])
     SolidBody ball_1(sph_system, makeShared<BallOne>("BallOne"));
     ball_1.defineBodyLevelSetShape();
     ball_1.defineParticlesAndMaterial<ElasticSolidParticles, ViscousPlasticSolid>(rho0_s, Youngs_modulus, poisson,
-    yield_stress, viscous_modulus, Herschel_Bulkley_power_1);
+    yield_stress, viscosity, Herschel_Bulkley_power_1);
     (!sph_system.RunParticleRelaxation() && sph_system.ReloadParticles())
         ? ball_1.generateParticles<ParticleGeneratorReload>(io_environment, ball_1.getName())
         : ball_1.generateParticles<ParticleGeneratorLattice>();
@@ -93,7 +92,7 @@ int main(int ac, char *av[])
     SolidBody ball_2(sph_system, makeShared<BallTwo>("BallTwo"));
     ball_2.defineBodyLevelSetShape();
     ball_2.defineParticlesAndMaterial<ElasticSolidParticles, ViscousPlasticSolid>(rho0_s, Youngs_modulus, poisson,
-    yield_stress, viscous_modulus, Herschel_Bulkley_power_2);
+    yield_stress, viscosity, Herschel_Bulkley_power_2);
     (!sph_system.RunParticleRelaxation() && sph_system.ReloadParticles())
         ? ball_2.generateParticles<ParticleGeneratorReload>(io_environment, ball_2.getName())
         : ball_2.generateParticles<ParticleGeneratorLattice>();
@@ -199,7 +198,7 @@ int main(int ac, char *av[])
     //	Setup for time-stepping control
     //----------------------------------------------------------------------
     int ite = 0;
-    Real T0 = 2.5; 
+    Real T0 = 3.0; 
     Real end_time = T0;
     Real output_interval = 0.01 * T0;
     Real Dt = 0.1 * output_interval;
