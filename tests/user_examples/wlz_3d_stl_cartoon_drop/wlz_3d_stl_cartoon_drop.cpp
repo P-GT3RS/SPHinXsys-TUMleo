751ccfb2846813cc2b426b022f7cd1232d648f6b
PGT3RS <liezhaors6@163.com>
2023-08-20 10:39:28 +0200

    1 cartoon with oobleck material

--- tests/user_examples/wlz_3d_stl_cartoon_drop/wlz_3d_stl_cartoon_drop.cpp
+++ tests/user_examples/wlz_3d_stl_cartoon_drop/wlz_3d_stl_cartoon_drop.cpp
@@ -1,52 +1,55 @@
 /**
- * @file 	collision_viscoplastic_3D.cpp
- * @brief 	one ball with viscoplastic or oobleck material bouncing with a boundary
- * @details
+ * @file 	stl_cartoon_drop_3D.cpp
+ * @details a cartoon bodies from .stl file with oobleck material bouncing with a boundary wall
  * @author 	Liezhao Wu, Chi Zhang and Xiangyu Hu
  */
-#include "sphinxsys.h" // SPHinXsys Library.
-using namespace SPH;   // Namespace cite here.
+#include "sphinxsys.h" /* SPHinXsys Library. */
+using namespace SPH;   /* Namespace cite here. */
+//----------------------------------------------------------------------
+//	Set the file path to the data file.
+//----------------------------------------------------------------------
+std::string full_path_to_file = "./input/Cartoon.stl";
 //----------------------------------------------------------------------
 //	Basic geometry parameters and numerical setup.
 //----------------------------------------------------------------------
-Real DL = 0.2; 
-Real DH = 0.6; 
-Real resolution_ref = 0.005;   
+Real DL = 0.5;
+Real DH = 0.5;
+Real resolution_ref = 0.003;
 Real BW = resolution_ref * 4;
+/** Domain bounds of the system. */
 BoundingBox system_domain_bounds(Vec3d(-DL, -DL, -BW), Vec3d(DL, DL, DH));
-Real ball_radius = resolution_ref * 10;
 //----------------------------------------------------------------------
-//	Global parameters on material properties
+//	Global parameters for material properties.
 //----------------------------------------------------------------------
-Real gravity_g = 1.0;
+Real gravity_g = 2.0;
 Real rho0_s = 1.0e3;                                                                                    /* ρ density. kg/m^3 */
 Real Bulk_modulus = 1.09e5;                                                                             /* κ bulk modulus. Pa */
 Real Shear_modulus = 1.12e4;                                                                            /* μ/G shear modulus. Pa */
+Real Youngs_modulus = (9.0 * Shear_modulus * Bulk_modulus) / (3.0 * Bulk_modulus + Shear_modulus);      /* E Young's modulus. Pa */
+Real poisson = (3.0 * Bulk_modulus - 2.0 * Shear_modulus) / (6.0 * Bulk_modulus + 2.0 * Shear_modulus); /* ν Poisson's ratio. */
 Real yield_stress = 0.1;                                                                                /* σ_Y yield stress. Pa */
-Real viscous_modulus = 10.0;                                                                            /* η viscosity.  */
-Real Youngs_modulus = (9.0 * Shear_modulus * Bulk_modulus) / (3.0 * Bulk_modulus + Shear_modulus);      /* E Young's modulus. Pa. 此处3.25e4 */
-Real poisson = (3.0 * Bulk_modulus - 2.0 * Shear_modulus) / (6.0 * Bulk_modulus + 2.0 * Shear_modulus); /* ν Poisson's ratio. 取值(-1,0.5). 此处0.45 */
-Real Herschel_Bulkley_power = 1.0; // oobleck with 2.8 or viscoplastic with 1.0                         /* h Herschel_Bulkley_power. */
+Real viscosity = 10.0;                                                                                  /* η viscosity.  */
+Real Herschel_Bulkley_power = 2.8;                                                                    /* viscoplastic material. */
 //----------------------------------------------------------------------
-//	Geometric shapes
+//	Body shapes used in the case.
 //----------------------------------------------------------------------
+class Cartoon : public ComplexShape
+{
+  public:
+    explicit Cartoon(const std::string &shape_name) : ComplexShape(shape_name)
+    {
+        Vec3d translation_1(0.0, 0.0, 0.75 * DH);
+        add<TriangleMeshShapeSTL>(full_path_to_file, translation_1, 0.01);
+    }
+};
 class WallBoundary : public ComplexShape
 {
   public:
     explicit WallBoundary(const std::string &shape_name) : ComplexShape(shape_name)
     {
-        Vec3d halfsize_wall(DL, DL, BW / 2.0);     
-        Vec3d translation_wall(0.0, 0.0, 0.0); 
-        add<TriangleMeshShapeBrick>(halfsize_wall, 10, translation_wall);
-    }
-};
-class BallBody : public ComplexShape
-{
-  public:
-    explicit BallBody(const std::string &shape_name) : ComplexShape(shape_name)
-    {
-        Vec3d translation_ball(0.0, 0.0, 0.5 * DH);
-        add<TriangleMeshShapeSphere>(ball_radius, 5, translation_ball);
+        Vecd halfsize_wall(0.5 * DL, 0.5 * DL, BW / 2.0);
+        Vecd translation_wall(0.0, 0.0, 0.0);
+        add<TriangleMeshShapeBrick>(halfsize_wall, 5, translation_wall);
     }
 };
 //----------------------------------------------------------------------
@@ -59,22 +62,23 @@ int main(int ac, char *av[])
     //----------------------------------------------------------------------
     SPHSystem sph_system(system_domain_bounds, resolution_ref);
     /** Tag for running particle relaxation for the initially body-fitted distribution */
-    sph_system.setRunParticleRelaxation(false); // true
+    sph_system.setRunParticleRelaxation(false); /* true/false */
     /** Tag for starting with relaxed body-fitted particles distribution */
-    sph_system.setReloadParticles(true); // false
+    sph_system.setReloadParticles(true);
+#ifdef BOOST_AVAILABLE
     sph_system.handleCommandlineOptions(ac, av);
+#endif
     IOEnvironment io_environment(sph_system);
     //----------------------------------------------------------------------
     //	Creating body, materials and particles.
     //----------------------------------------------------------------------
-    // SolidBody ball(sph_system, makeShared<GeometricShapeBall>(ball_center, ball_radius, "BallBody"));
-    SolidBody ball(sph_system, makeShared<BallBody>("BallBody"));
-    ball.defineBodyLevelSetShape();
-    ball.defineParticlesAndMaterial<ElasticSolidParticles, ViscousPlasticSolid>(rho0_s, Youngs_modulus, poisson,
-    yield_stress, viscous_modulus, Herschel_Bulkley_power);
+    SolidBody cartoon(sph_system, makeShared<Cartoon>("Cartoon"));
+    cartoon.defineBodyLevelSetShape();
+    cartoon.defineParticlesAndMaterial<ElasticSolidParticles, ViscousPlasticSolid>(rho0_s, Youngs_modulus, poisson,
+                                                                                     yield_stress, viscosity, Herschel_Bulkley_power);
     (!sph_system.RunParticleRelaxation() && sph_system.ReloadParticles())
-        ? ball.generateParticles<ParticleGeneratorReload>(io_environment, ball.getName())
-        : ball.generateParticles<ParticleGeneratorLattice>();
+        ? cartoon.generateParticles<ParticleGeneratorReload>(io_environment, cartoon.getName())
+        : cartoon.generateParticles<ParticleGeneratorLattice>();
 
     SolidBody wall_boundary(sph_system, makeShared<WallBoundary>("WallBoundary"));
     wall_boundary.defineParticlesAndMaterial<SolidParticles, Solid>(rho0_s, Youngs_modulus, poisson);
@@ -87,62 +91,71 @@ int main(int ac, char *av[])
         //----------------------------------------------------------------------
         //	Define body relation map used for particle relaxation.
         //----------------------------------------------------------------------
-        InnerRelation ball_inner(ball);
+        InnerRelation cartoon_inner(cartoon);
         //----------------------------------------------------------------------
         //	Define the methods for particle relaxation for ball.
         //----------------------------------------------------------------------
-        SimpleDynamics<RandomizeParticlePosition> ball_random_particles(ball);
-        relax_dynamics::RelaxationStepInner ball_relaxation_step_inner(ball_inner);
+        SimpleDynamics<RandomizeParticlePosition> random_cartoon_particles(cartoon);
+        relax_dynamics::RelaxationStepInner relaxation_step_inner(cartoon_inner);
         //----------------------------------------------------------------------
         //	Output for particle relaxation.
         //----------------------------------------------------------------------
-        BodyStatesRecordingToVtp write_ball_state(io_environment, sph_system.real_bodies_);
-        ReloadParticleIO write_particle_reload_files(io_environment, ball);
+        BodyStatesRecordingToVtp write_states(io_environment, sph_system.real_bodies_);
+        ReloadParticleIO write_particle_reload_files(io_environment, cartoon);
         //----------------------------------------------------------------------
         //	Particle relaxation starts here.
         //----------------------------------------------------------------------
-        ball_random_particles.exec(0.25);
-        write_ball_state.writeToFile(0);
+        random_cartoon_particles.exec(0.25);
+        relaxation_step_inner.SurfaceBounding().exec();
+        write_states.writeToFile(0);
         //----------------------------------------------------------------------
         //	From here iteration for particle relaxation begins.
         //----------------------------------------------------------------------
-        int ite = 0;
-        int relax_step = 1000;
-        while (ite < relax_step)
+        int ite_p = 0;
+        while (ite_p < 1000)
         {
-            ball_relaxation_step_inner.exec();
-            ite += 1;
-            if (ite % 100 == 0)
+            relaxation_step_inner.exec();
+            ite_p += 1;
+            if (ite_p % 100 == 0)
             {
-                std::cout << std::fixed << std::setprecision(9) << "Relaxation steps N = " << ite << "\n";
-                write_ball_state.writeToFile(ite);
+                std::cout << std::fixed << std::setprecision(9) << "Relaxation steps for the inserted body N = " << ite_p << "\n";
+                write_states.writeToFile(ite_p);
             }
         }
-        std::cout << "The physics relaxation process of ball particles finish !" << std::endl;
+        std::cout << "The physics relaxation process of inserted body finish !" << std::endl;
+        // Output particles position for reload.
         write_particle_reload_files.writeToFile(0);
         return 0;
     }
     //----------------------------------------------------------------------
     //	Define body relation map.
     //	The contact map gives the topological connections between the bodies.
-    //	Basically the range of bodies to build neighbor particle lists.
+    //	Basically the the range of bodies to build neighbor particle lists.
     //----------------------------------------------------------------------
-    InnerRelation ball_inner(ball);
-    SurfaceContactRelation ball_contact(ball, {&wall_boundary});
+    InnerRelation cartoon_inner(cartoon);
+    SelfSurfaceContactRelation cartoon_self_contact(cartoon);
+    SurfaceContactRelation cartoon_contact(cartoon_self_contact, {&wall_boundary});
     //----------------------------------------------------------------------
     //	Define the main numerical methods used in the simulation.
     //	Note that there may be data dependence on the constructors of these methods.
     //----------------------------------------------------------------------
+    // initialize a time step
     SharedPtr<Gravity> gravity_ptr = makeShared<Gravity>(Vec3d(0.0, 0.0, -gravity_g));
-    SimpleDynamics<TimeStepInitialization> ball_initialize_timestep(ball, gravity_ptr);
-    InteractionWithUpdate<CorrectedConfigurationInner> ball_corrected_configuration(ball_inner);
-    ReduceDynamics<solid_dynamics::AcousticTimeStepSize> ball_get_time_step_size(ball, 0.1);
-    /** stress relaxation for the ball. */
-    Dynamics1Level<solid_dynamics::PlasticIntegration1stHalf> ball_stress_relaxation_first_half(ball_inner);
-    Dynamics1Level<solid_dynamics::Integration2ndHalf> ball_stress_relaxation_second_half(ball_inner);
-    /** Algorithms for solid-solid contact. */
-    InteractionDynamics<solid_dynamics::ContactDensitySummation> ball_update_contact_density(ball_contact);
-    InteractionDynamics<solid_dynamics::ContactForceFromWall> ball_compute_solid_contact_forces(ball_contact);
+    SimpleDynamics<TimeStepInitialization> initialization_with_gravity(cartoon, gravity_ptr);
+    // Corrected configuration for reproducing rigid rotation.
+    InteractionWithUpdate<CorrectedConfigurationInner> corrected_configuration(cartoon_inner);
+    // Time step size
+    ReduceDynamics<solid_dynamics::AcousticTimeStepSize> computing_time_step_size(cartoon, 0.1);
+    // stress relaxation for the ball.
+    Dynamics1Level<solid_dynamics::PlasticIntegration1stHalf> stress_relaxation_first_half(cartoon_inner);
+    Dynamics1Level<solid_dynamics::Integration2ndHalf> stress_relaxation_second_half(cartoon_inner);
+    // Algorithms for solid-solid contacts.
+    InteractionDynamics<solid_dynamics::ContactDensitySummation> cartoon_update_contact_density(cartoon_contact);
+    InteractionDynamics<solid_dynamics::ContactForceFromWall> cartoon_compute_solid_contact_forces(cartoon_contact);
+    InteractionDynamics<solid_dynamics::SelfContactDensitySummation> cartoon_self_contact_density(cartoon_self_contact);
+    InteractionDynamics<solid_dynamics::SelfContactForce> cartoon_self_contact_forces(cartoon_self_contact);
+
+    SimpleDynamics<solid_dynamics::ConstrainSolidBodyMassCenter> constrain_mass_center(cartoon, Vec3d(1.0, 1.0, 0.0));
     //----------------------------------------------------------------------
     //	Define the methods for I/O operations and observations of the simulation.
     //----------------------------------------------------------------------
@@ -153,7 +166,8 @@ int main(int ac, char *av[])
     //----------------------------------------------------------------------
     sph_system.initializeSystemCellLinkedLists();
     sph_system.initializeSystemConfigurations();
-    ball_corrected_configuration.exec();
+    // apply initial condition
+    corrected_configuration.exec();
     //----------------------------------------------------------------------
     //	Initial states output.
     //----------------------------------------------------------------------
@@ -164,41 +178,45 @@ int main(int ac, char *av[])
     int ite = 0;
     Real T0 = 3.0;
     Real end_time = T0;
-    Real output_interval = 0.01 * T0;
-    Real Dt = 0.1 * output_interval;
+    Real output_period = 0.01 * T0;
+    Real Dt = 0.1 * output_period;
     Real dt = 0.0;
-    //----------------------------------------------------------------------
-    //	Statistics for CPU time
-    //----------------------------------------------------------------------
+    // Statistics for computing time.
     TickCount t1 = TickCount::now();
     TimeInterval interval;
     //----------------------------------------------------------------------
-    //	Main loop starts here.
+    //	Main loop
     //----------------------------------------------------------------------
     while (GlobalStaticVariables::physical_time_ < end_time)
     {
         Real integration_time = 0.0;
-        while (integration_time < output_interval)
+        while (integration_time < output_period)
         {
             Real relaxation_time = 0.0;
             while (relaxation_time < Dt)
             {
-                ball_initialize_timestep.exec();
+                initialization_with_gravity.exec();
                 if (ite % 100 == 0)
                 {
                     std::cout << "N=" << ite << " Time: "
-                              << GlobalStaticVariables::physical_time_ << " dt: " << dt << "\n";
+                              << GlobalStaticVariables::physical_time_ << "	dt: "
+                              << dt << "\n";
                 }
-                ball_update_contact_density.exec();
-                ball_compute_solid_contact_forces.exec();
-                ball_stress_relaxation_first_half.exec(dt);
-                ball_stress_relaxation_second_half.exec(dt);
-
-                ball.updateCellLinkedList();
-                ball_contact.updateConfiguration();
+                // contact dynamics.
+                cartoon_self_contact_density.exec();
+                cartoon_self_contact_forces.exec();
+                cartoon_update_contact_density.exec();
+                cartoon_compute_solid_contact_forces.exec();
+                // Stress relaxation and damping.
+                stress_relaxation_first_half.exec(dt);
+                stress_relaxation_second_half.exec(dt);
+                // update particle neighbor relations for contact dynamics
+                cartoon.updateCellLinkedList();
+                cartoon_self_contact.updateConfiguration();
+                cartoon_contact.updateConfiguration();
 
                 ite++;
-                dt = ball_get_time_step_size.exec();
+                dt = computing_time_step_size.exec();
                 relaxation_time += dt;
                 integration_time += dt;
                 GlobalStaticVariables::physical_time_ += dt;
