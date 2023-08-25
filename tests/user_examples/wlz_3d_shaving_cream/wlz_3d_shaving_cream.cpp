51455eb3536eb52e046f6b41265794f199d4e0f1
PGT3RS <liezhaors6@163.com>
2023-08-25 08:03:57 +0200

    finial work

--- tests/user_examples/wlz_3d_shaving_cream/wlz_3d_shaving_cream.cpp
+++ tests/user_examples/wlz_3d_shaving_cream/wlz_3d_shaving_cream.cpp
@@ -1,11 +1,10 @@
 /**
  * @file 	shaving_cream_drop_3D.cpp
- * @brief
- * @details
+ * @details shaving cream attached to the platform falls due to gravity
  * @author 	Liezhao Wu, Chi Zhang and Xiangyu Hu
  */
-#include "sphinxsys.h"
-using namespace SPH;
+#include "sphinxsys.h"         /* SPHinXsys Library. */
+using namespace SPH;           /* Namespace cite here. */
 //----------------------------------------------------------------------
 //	Set the file path to the data file.
 //----------------------------------------------------------------------
@@ -13,29 +12,24 @@ std::string full_path_to_file = "./input/shaving_foam.stl";
 //----------------------------------------------------------------------
 //	Basic geometry parameters and numerical setup.
 //----------------------------------------------------------------------
-Real DL = 0.2; 
-Real DH = 0.5;
-Real resolution_ref = 0.0025;  /* reference resolution. */
+Real resolution_ref = 0.015;  /* reference resolution. */
+Real DL = 1.0; 
+Real DH = 5.0;
 Real BW = resolution_ref * 4; /* width for BCs. */
-BoundingBox system_domain_bounds(Vec3d(-DL, -DL, 0.0), Vec3d(DL, DL, DH));
-
-Real start_time = 0.0;
-Real end_time = 5.0;
-Vec3d displacement(0.2, 0.0, 0.0);
-int frequency = 5;
+BoundingBox system_domain_bounds(Vec3d(-DL, -DL, -DH), Vec3d(DL, DL, DH));
 //----------------------------------------------------------------------
-//	Global parameters on material properties
+//	Global parameters on material properties.
 //----------------------------------------------------------------------
-// shaving cream
+Real gravity_g = 9.8;
+// shaving cream material
 Real rho0_s = 77.7;                                                                                     /* ¦Ñ density. kg/m^3 */
 Real Bulk_modulus = 1.09e5;                                                                             /* ¦Ê bulk modulus. Pa */
 Real Shear_modulus = 290.0;                                                                             /* ¦Ì/G shear modulus. Pa */
 Real yield_stress = 31.9;                                                                               /* ¦Ò_Y yield stress. Pa */
-Real viscous_modulus = 27.2;                                                                            /* ¦Ç viscosity. */
+Real viscosity = 27.2;                                                                                  /* ¦Ç viscosity. */
 Real Herschel_Bulkley_power = 0.22;                                                                     /* h Herschel_Bulkley_power. */
 Real Youngs_modulus = (9.0 * Shear_modulus * Bulk_modulus) / (3.0 * Bulk_modulus + Shear_modulus);      /* E Young's modulus. Pa */
-Real poisson = (3.0 * Bulk_modulus - 2.0 * Shear_modulus) / (6.0 * Bulk_modulus + 2.0 * Shear_modulus); /* Poisson's ratio. */
-Real gravity_g = 9.8;
+Real poisson = (3.0 * Bulk_modulus - 2.0 * Shear_modulus) / (6.0 * Bulk_modulus + 2.0 * Shear_modulus); /* ¦Í Poisson's ratio. */
 //----------------------------------------------------------------------
 //	Geometries used in the case.
 //----------------------------------------------------------------------
@@ -44,8 +38,8 @@ class Cream : public ComplexShape
   public:
     explicit Cream(const std::string &shape_name) : ComplexShape(shape_name)
     {
-        Vec3d translation(0.0, 0.0, DH / 2.0);
-        add<TriangleMeshShapeSTL>(full_path_to_file, translation, 0.02);
+        Vec3d translation_cream(0.0, 0.0, DH / 2.0);
+        add<TriangleMeshShapeSTL>(full_path_to_file, translation_cream, 0.2);
     }
 };
 //----------------------------------------------------------------------
@@ -69,7 +63,7 @@ int main(int ac, char *av[])
     SolidBody cream(sph_system, makeShared<Cream>("Cream"));
     cream.defineBodyLevelSetShape();
     cream.defineParticlesAndMaterial<ElasticSolidParticles, ViscousPlasticSolid>(rho0_s, Youngs_modulus, poisson,
-                                                                                 yield_stress, viscous_modulus, Herschel_Bulkley_power);
+                                                                                 yield_stress, viscosity, Herschel_Bulkley_power);
     (!sph_system.RunParticleRelaxation() && sph_system.ReloadParticles())
         ? cream.generateParticles<ParticleGeneratorReload>(io_environment, cream.getName())
         : cream.generateParticles<ParticleGeneratorLattice>();
@@ -129,16 +123,15 @@ int main(int ac, char *av[])
     SharedPtr<Gravity> gravity_ptr = makeShared<Gravity>(Vec3d(0.0, 0.0, -gravity_g));
     SimpleDynamics<TimeStepInitialization> cream_initialize_gravity(cream, gravity_ptr);
     InteractionWithUpdate<CorrectedConfigurationInner> cream_corrected_configuration(cream_inner);
-    ReduceDynamics<solid_dynamics::AcousticTimeStepSize> cream_get_time_step_size(cream, 0.5);
+    ReduceDynamics<solid_dynamics::AcousticTimeStepSize> cream_get_time_step_size(cream, 0.2);
     /** stress relaxation for the foam. */
     Dynamics1Level<solid_dynamics::PlasticIntegration1stHalf> cream_stress_relaxation_first_half(cream_inner);
     Dynamics1Level<solid_dynamics::Integration2ndHalf> cream_stress_relaxation_second_half(cream_inner);
 
-    Vec3d halfsize(DL, DL, 0.025);
+    Vec3d halfsize(DL, DL, 0.05);
     Vec3d translation(0.0, 0.0, DH / 2.0);
     BodyRegionByParticle wall_boundary(cream, makeShared<TriangleMeshShapeBrick>(halfsize, 10, translation));
     SimpleDynamics<solid_dynamics::FixBodyPartConstraint> wall_constraint(wall_boundary);
-    SimpleDynamics<solid_dynamics::ForthAndBackMotionBodyPart> wall_initialize_motion(wall_boundary, start_time, end_time, displacement, frequency);
     //----------------------------------------------------------------------
     //	Define the methods for I/O operations and observations of the simulation.
     //----------------------------------------------------------------------
@@ -158,7 +151,7 @@ int main(int ac, char *av[])
     //	Setup for time-stepping control
     //----------------------------------------------------------------------
     int ite = 0;
-    Real T0 = 5.0;
+    Real T0 = 1.0;
     Real end_time = T0;
     Real output_interval = 0.01 * T0;
     Real Dt = 0.1 * output_interval;
